92102e03552890491ff5db18ad5c99bc6666f400
sofiawong <sofiawong1999@gmail.com>
2018-03-10 22:57:43 +0800

    Add routers as parameters for different routing

--- node-express/index.js
+++ node-express/index.js
@@ -4,6 +4,8 @@ const morgan = require ('morgan');
 const bodyParser = require ('body-parser');
 
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 const hostname = 'localhost';
 const port = 3000;
 
@@ -12,6 +14,8 @@ app.use(morgan('dev'));
 app.use(bodyParser.json()); // Parse body of request msg which is in json format
 
 app.use('/dishes' , dishRouter);
+app.use('/promotions' , promoRouter);
+app.use('/leaders' , leaderRouter);
 
 // Any request coming to this end point will be handled by dishRouter
 const server = http.createServer(app);
