0cc21fcf361b3aaa614162aa42f11fd050847526
sofiawong <sofiawong1999@gmail.com>
2018-03-10 17:56:35 +0800

    Completed Task 1 of Assignment

--- node-express/index.js
+++ node-express/index.js
@@ -11,28 +11,10 @@ const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json()); // Parse body of request msg which is in json format
 
-app.use('/dishes', dishRouter);
+app.use('/dishes' , dishRouter);
+
 // Any request coming to this end point will be handled by dishRouter
-app.get('/dishes/:dishId' , (req,res,next) => {
-  res.end('Will send details of the dish: ' + req.params.dishId  +
-  'to you!');
-});
-
-app.post('/dishes/:dishId' , (req,res,next) => {
-  res.statusCode = 403;
-  res.end('POST operation not supported on /dishes/'
-  + req.params.dishId);
-});
-
-app.put('/dishes/:dishId' , (req,res,next) => {
-  res.write('Updating the dish: ' + req.params.dishId + '\n');
-  res.end('Will update the dish: ' + req.body.name + 'with details: ' +
-  req.body.description);
-});
-
-app.delete('/dishes/:dishId' , (req,res,next) => {
-  res.end('Deleting dish' + req.params.dishId);
-});
+const server = http.createServer(app);
 
 app.use(express.static(__dirname+ '/public'));
 
@@ -41,8 +23,6 @@ app.use((req,res, next) => {
   res.setHeader('Content-Type','text/html');
   res.end('<html><body><h1>This is an express server</h1></body></html>');
 });
-
-const server = http.createServer(app);
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}`);
