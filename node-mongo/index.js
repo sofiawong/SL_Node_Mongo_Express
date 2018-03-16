436850b4a344be35184bd597a7a5257ec8735e20
sofiawong <sofiawong1999@gmail.com>
2018-03-17 00:36:52 +0800

    Add reference link for collection.drop

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -2,35 +2,40 @@ const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
 const dboper = require('./operations');
+
 const url = 'mongodb://localhost:27017/conFusion';
 
-
+// db.collection is not a function, https://stackoverflow.com/questions/43779323/typeerror-db-collection-is-not-a-function
+// https://mongodb.github.io/mongo-csharp-driver/2.5/apidocs/html/M_MongoDB_Driver_MongoCollection_Drop.htm
 MongoClient.connect(url, (err, client) => {
-  
   const conFusionDatabase = client.db('myDatabaseNameAsAString');
-  assert.equal(err,null);
+  assert.equal(err, null);
 
   console.log('Connected correctly to server');
-  
-  dboper.insertDocument(client, {name: "Vadonut", description: "Test"} ,
-            "dishes", (result) => {
-              console.log("Insert Document:\n", result.ops);
-              dboper.findDocuments(client, "dishes", (docs) => {
-               console.log("Found Documents:\n", docs);
-               dboper.updateDocument(client, {name: "Vadonut"} , {description: "Updated Test"}
-               , "dishes", (results) => {
-                 console.log("Updated Document:\n", result.result);
-                 dboper.findDocuments(client, "dishes", (docs) => {
-                  console.log("Found Updated Documents:\n", docs);
-                  const collection = conFusionDatabase.collection("dishes");
-                  collection.drop((result) => {
-                   console.log("Dropped Collection: " , result);
-                   client.close();
-                  });
-                 });
-               });
+
+  dboper.insertDocument(
+client, { name: 'Vadonut', description: 'Test' },
+    'dishes', (result) => {
+      console.log('Insert Document:\n', result.ops);
+      dboper.findDocuments(client, 'dishes', (docs) => {
+        console.log('Found Documents:\n', docs);
+        dboper.updateDocument(
+client, { name: 'Vadonut' }, { description: 'Updated Test' }
+          , 'dishes', (results) => {
+            console.log('Updated Document:\n', result.result);
+            dboper.findDocuments(client, 'dishes', (docs) => {
+              console.log('Found Updated Documents:\n', docs);
+              const collection = conFusionDatabase.collection('dishes');
+              collection.drop((result) => {
+                console.log('Dropped Collection: ', result);
+                client.close();
               });
             });
+          }
+);
+      });
+    }
+);
   /* const collection = conFusionDatabase.collection("dishes");
   collection.insertOne({"name": "Uthappizza", "description": "test"},
     (err, result) => {
@@ -41,7 +46,7 @@ MongoClient.connect(url, (err, client) => {
 
       collection.find({}).toArray((err, docs) => {
         assert.equal(err, null);
-            
+
         console.log("Found:\n");
         console.log(docs);
 
