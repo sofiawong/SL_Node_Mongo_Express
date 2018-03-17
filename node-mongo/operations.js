65da736441c0553a363505a95c5e476876b3d09b
sofiawong <sofiawong1999@gmail.com>
2018-03-17 16:36:24 +0800

    Modified some codes to work with promises in index.js

--- node-mongo/operations.js
+++ node-mongo/operations.js
@@ -3,41 +3,25 @@ const assert = require('assert');
 exports.insertDocument = (client, document, collection, callback) => {
     const conFusionDatabase = client.db('myDatabaseNameAsAString');
     const coll = conFusionDatabase.collection("dishes");
-    coll.insert(document, (err, result) => {
-        assert.equal(err,null);
-        console.log("Inserted " + result.result.n + 
-        "documents into the collection " + collection);
-        callback(result);
-    });
+    return coll.insert(document);
 };
 
 exports.findDocuments = (client, collection, callback) => {
     const conFusionDatabase = client.db('myDatabaseNameAsAString');
     const coll = conFusionDatabase.collection("dishes");
-    coll.find({}).toArray((err,docs) => {
-    assert.equal(err,null);
-    callback(docs);
-    });
+    return coll.find({}).toArray();
 };
 
 exports.removeDocument = (client, document, collection, callback) => {
     const conFusionDatabase = client.db('myDatabaseNameAsAString');
     const coll = conFusionDatabase.collection("dishes");
-    coll.deleteOne(document, (err, result) => {
-    assert.equal(err, null);
-    console.log("Removed the document ", document);
-    callback(result);
-    });
+    return coll.deleteOne(document);
 };
 
 exports.updateDocument = (client, document, update, collection, callback) => {
     const conFusionDatabase = client.db('myDatabaseNameAsAString');
     const coll = conFusionDatabase.collection("dishes");
-    coll.updateOne(document, {$set: update} , null, (err, result) => {
-        assert.equal(err,null);
-        console.log("Update the document with", update);
-        callback(result);
-    });
+    return coll.updateOne(document, {$set: update} , null);
 };
 
 
