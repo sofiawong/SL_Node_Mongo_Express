afaa4250507237e843945db2fdb99f914d3aa286
sofiawong <sofiawong1999@gmail.com>
2018-03-26 22:50:10 +0800

    Complete Task 1 of Assignment

--- conFusionServer/authenticate.js
+++ conFusionServer/authenticate.js
@@ -21,19 +21,29 @@ opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = config.secretKey;
 
 exports.jwtPassport = passport.use(new JwtStrategy(opts,
-    (jwt_payload, done) => {
-        console.log("JWT payload: ", jwt_payload);
-        User.findOne({_id: jwt_payload._id}, (err, user) => {
-            if (err) {
-                return done(err, false);
-            }
-            else if (user) {
-                return done(null, user);
-            }
-            else {
-                return done(null, false);
-            }
-        });
-    }));
+  (jwt_payload, done) => {
+    console.log("JWT payload: ", jwt_payload);
+    User.findOne({_id: jwt_payload._id}, (err, user) => {
+      if (err) {
+        return done(err, false);
+      }
+      else if (user) {
+        return done(null, user);
+      }
+      else {
+        return done(null, false);
+      }
+    });
+  }));
 
 exports.verifyUser = passport.authenticate('jwt', {session: false});
+
+exports.verifyAdmin = (req, res, next) => {
+  if (!req.user.admin) {
+    err = new Error('You are not authorised to perform this action');
+    err.statusCode = 403;
+    return next(err);
+  } else {
+    next();
+  }
+};
