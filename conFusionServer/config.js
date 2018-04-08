caad01cce99a2a499bae3467b0127698cbc251ff
sofiawong <sofiawong1999@gmail.com>
2018-04-08 18:57:51 +0800

    LATE PUSH: Completed OAuth and other tasks

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '573691896330143',
+        clientSecret: '97eb4fe4f577c66b5962dcd7995635fa'
+    }
 }
